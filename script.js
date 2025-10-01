d9c18df77326c3a288d8a4cde94a40099a65fa96
akilfoil26 <akilfoil26@students.stab.org>
2025-09-30 21:02:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
-function getQuote (){
-    var landscapes = document.getElementById("landscapeFamily").value;
-    var attractions= document.getElementById("attractionFamily").value;
-    var feedback = document.getElementById("feedback").value;
-   
+function showCountry (landscapes, attractions, size){
+ 
+    var feedback; 
 
     console.log(landscapes);
     console.log(attractions);
-    console.log(feedback);
-
-    //putting what i write into the output box into the "quote" box
-    document.getElementById("output").innerHTML=landscapes;
-    //taking what i write into the output box and setting it equal to what the font is
-    document.getElementById("output").style.fontFamily=attractions;
-
+    console.log(size);
     
-    if (boxColor=="lavender" && font=="Brush Script MT" ){
-       feedback="Great combo!"
+    if (landscapes=="Land" && attractions=="Architecture" && (size>=50 && size<=70)){
+       feedback="Germany & Ireland & Britain & the Netherlands"
+    }
+    else if (landscapes=="Land" && attractions=="Architecture" && (size>70 && size<=90)){
+       feedback="Spain & Portugal & France & Belgium & Austria"
+    }
+    else if (landscapes=="Land" && attractions=="Historical Landmarks" && (size>=65 && size<=75)){
+       feedback="Germany"  
+     }
+   else if (landscapes=="Land" && attractions=="Nature features" && (size>=50 && size<=70)){
+       feedback="Ireland & the Netherlands & Britain & Ireland"
+    }
+     else if (landscapes=="Land" && attractions=="Nature features" && (size>70 && size<=90)){
+       feedback="Spain & Portugal"
+    }
+    else if (landscapes=="Water" && attractions=="Architecture" && (size>=50 && size<=70)){
+       feedback="Ireland & Britain"
+     }
+     else if (landscapes=="Water" && attractions=="Architecture" && (size>70 && size<=90)){
+       feedback="France & Italy & Portugal & Spain"
+     }
+   else if (landscapes=="Water" && attractions=="Historical Landmarks" && (size>=60 && size<=63)){
+       feedback="Isle of Man" 
+    }
+    else if (landscapes=="Water" && attractions=="Historical Landmarks" && (size>=70 && size<=90)){
+       feedback="Italy" 
+    }
+     
+   else if (landscapes=="Water" && attractions=="Nature features" && (size>=50 && size<=70)){
+       feedback="Isle of Man & Ireland & Britain & the Netherlands"
+
     } 
-
-    if (boxColor=="lightgreen"){
-        feedback="Looking Fresh!"
+     else if (landscapes=="Water" && attractions=="Nature features" && (size>70 && size<=90)){
+       feedback="Spain & Portugal & Monacco"
+     } else{
+        feedback = "Your country is..."
     }
 
-    else if (boxColor=="lightblue" || boxColor=="gold" || boxColor=="lightpink"){
-        feedback="Nice design"
-    }
 
-    document.getElementById("feedback").innerHTML=feedback
+    console.log(feedback);
+    return feedback;
 }
+// showCountry()
+
+
+
 function showTemp(){
     var size= document.getElementById("size").value;
     document.getElementById("feedback").innerHTML= size +" °F"
@@ -35,5 +59,11 @@ function showTemp(){
 
 showTemp();
 
+
+
+
+function getResults(){
+     document.getElementById("result").innerHTML= showCountry(document.getElementById("landscapeFamily").value, document.getElementById("attractionFamily").value, Number(document.getElementById("size").value));
+}
 
 
